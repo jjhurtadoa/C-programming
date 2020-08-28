17fb6c670553f62f4aa58f3dc64a23a54b7ac70a
Coursera Learner <nobody@nowhere.nul>
2020-08-28 20:46:37 +0000

    jola

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int find_max(int size1, int size2) {
+int max(int size1, int size2) {
   if (size1 > size2) {
     return size1;
   }
@@ -42,9 +42,9 @@ int isAtBorder(int coord, int offset, int size) {
 
 void squares(int size1, int x_offset, int y_offset, int size2) {
   //compute the max of size1 and (x_offset + size2).  Call this w
-  int w = find_max(size1, x_offset + size2);
+  int w = max(size1, x_offset + size2);
   //compute the max of size1 and (y_offset + size2).  Call this h
-  int h = find_max(size1, y_offset + size2);
+  int h = max(size1, y_offset + size2);
   //count from 0 to h. Call the number you count with y
   for (int y = 0; y < h; ++y) {
     //count from 0 to w. Call the number you count with x
@@ -56,7 +56,7 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       //    ((y is between y_offset and y_offset + size2) AND
       //     x is equal to either x_offset OR x_offset + size2 -1)
       // if so, print a *
-      if (((isInRange( x,  x_offset,  size2)) &&( isAtBorder(y, y_offset, size2 -1)))||((isInRange( y,  y_offset,  size2))&&(isAtBorder(x, x_offset, size2 -1)))) {
+      if (((x_offset<=x && x<x_offset +size2) && (y==y_offset || y==y_offset + size2 - 1)) || ((y_offset<=y && y<y_offset +size2) && ((x==x_offset || x==x_offset +size2 - 1)))) {
 	  	printf("*");	
       //if not,
       // check if EITHER
@@ -74,7 +74,11 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
       else {
 	printf(" ");
       }
+    }
 
       printf("\n");
+    
+  }
 }
-  }}
+
+  
