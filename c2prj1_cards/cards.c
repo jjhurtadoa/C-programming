07aa2a83527af1b47088cdc4f23a9fa4990d7127
Coursera Learner <nobody@nowhere.nul>
2020-09-01 02:07:21 +0000

    oe

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,34 +5,88 @@
 
 
 void assert_card_valid(card_t c) {
-
+  assert(c.value >= 2 && c.value <= VALUE_ACE);
+  assert(c.suit >= SPADES && c.suit <= CLUBS);
 
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  switch(r){
+  case STRAIGHT_FLUSH: return "STRAIGHT_FLUSH";
+  case FOUR_OF_A_KIND: return "FOUR_OF_A_KIND";
+  case FULL_HOUSE: return "FULL_HOUSE";
+  case FLUSH: return "FLUSH";
+  case STRAIGHT: return "STRAIGHT";
+  case THREE_OF_A_KIND: return "THREE_OF_A_KIND";
+  case TWO_PAIR: return "TWO_PAIR";
+  case PAIR: return "PAIR";
+  case NOTHING: return "NOTHING";
+  default: return "";
+  }
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  switch(c.value){
+  case 2: return '2';
+  case 3: return '3';
+  case 4: return '4';
+  case 5: return '5';
+  case 6: return '6';
+  case 7: return '7';
+  case 8: return '8';
+  case 9: return '9';
+  case VALUE_JACK : return 'J';
+  case VALUE_QUEEN : return 'Q';
+  case VALUE_KING : return 'K'; 
+  case VALUE_ACE : return 'A';
+  default: return  24;
+  }
 }
 
 
 char suit_letter(card_t c) {
-  return 'x';
-  
+  switch(c.suit){
+  case SPADES: return 's';
+  case HEARTS: return 'h';
+  case DIAMONDS: return 'd';
+  case CLUBS: return 'c';
+  default: return 'x';
+  }
+   
 }
 
 void print_card(card_t c) {
-
+  printf("%c%c",value_letter(c),suit_letter(c));
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
+  if(value_let >='2' && value_let<='9'){
+    temp.value=value_let-'0';}
+  else if (value_let =='0'){
+    temp.value = 10;}
+  else if(value_let=='J'){
+    temp.value= VALUE_JACK;}
+  else if(value_let=='Q'){
+    temp.value= VALUE_QUEEN;}
+  else if(value_let=='K'){
+    temp.value= VALUE_KING;}
+  else if(value_let=='A'){
+    temp.value= VALUE_ACE;}
+
+  if(suit_let=='s'){temp.suit=SPADES;}
+  else if (suit_let=='h'){temp.suit=HEARTS;}
+  else if (suit_let=='d'){temp.suit=DIAMONDS;}
+  else if (suit_let=='c'){temp.suit=CLUBS;}
+
+  assert_card_valid(temp);
   return temp;
+  
 }
 
 card_t card_from_num(unsigned c) {
   card_t temp;
+  temp.value=(c/4)+2;
+  temp.suit=c%4;
   return temp;
 }
